8f91d69d19450ee173b76ad5c244fd4afc89e708
thebot002 <31898693+thebot002@users.noreply.github.com>
2017-10-06 17:12:35 +0200

    Base idea for Pentominoe object.

---
+++ Pentominoe.java
@@ -0,0 +1,21 @@
+public class Pentominoe {
+
+  private int[][] pentominoe = new int[5][5];
+
+  public Pentominoe(){
+
+  }
+  public Pentominoe(char[] listOfForms, int[] sizeOBoard){
+
+  }
+
+  public void rotate(int amountRotations){
+
+  }
+  public void flip(){
+
+  }
+  public int[][] hasFlatSideOn(){
+
+  }
+}
