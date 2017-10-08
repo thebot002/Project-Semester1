11fc98acf0cff98bc8b0dcfd3ddf9ecb4b2c1add
thebot002 <31898693+thebot002@users.noreply.github.com>
2017-10-08 21:16:12 +0200

    Adding of rotation & flip to Pentominoes

--- Pentominoe.java
+++ Pentominoe.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Pentominoe {
 
   private char[][] pentominoe;
@@ -157,5 +159,66 @@ public class Pentominoe {
   }
   public int getHeight(){
     return pentominoe.length;
+  }
+
+  public void rotateR(){ //clockwise
+    char[][] temp = new char[pentominoe[0].length][pentominoe.length];
+    for(int i=0; i<pentominoe.length;i++){
+      for(int j=0;j<pentominoe[0].length;j++){
+        temp[j][i] = pentominoe[i][j];
+      }
+    }
+    pentominoe = Arrays.copyOf(temp, temp.length);
+    flipH();
+  }
+  public void rotateL(){ //counter clockwise
+    char[][] temp = new char[pentominoe[0].length][pentominoe.length];
+    for(int i=0; i<pentominoe.length;i++){
+      for(int j=0;j<pentominoe[0].length;j++){
+        temp[j][i] = pentominoe[i][j];
+      }
+    }
+    pentominoe = Arrays.copyOf(temp, temp.length);
+    flipV();
+  }
+  public void flipH(){ //horizontal
+    if(pentominoe[0].length%2 != 0){
+      for(int i=0; i<pentominoe.length; i++){
+        for(int j=0;j<(pentominoe[0].length-1)/2;j++){
+          char temp = pentominoe[i][j];
+          pentominoe[i][j] = pentominoe[i][pentominoe[0].length-1-j];
+          pentominoe[i][pentominoe[0].length-1-j] = temp;
+        }
+      }
+    }
+    else{
+      for(int i=0; i<pentominoe.length; i++){
+        for(int j=0;j<pentominoe[0].length/2;j++){
+          char temp = pentominoe[i][j];
+          pentominoe[i][j] = pentominoe[i][pentominoe[0].length-1-j];
+          pentominoe[i][pentominoe[0].length-1-j] = temp;
+        }
+      }
+    }
+  }
+  public void flipV(){ //Vertical
+    if(pentominoe.length%2 != 0){
+      for(int i=0; i<(pentominoe.length-1)/2; i++){
+        for(int j=0;j<pentominoe[0].length;j++){
+          char temp = pentominoe[i][j];
+          pentominoe[i][j] = pentominoe[pentominoe.length-1-i][j];
+          pentominoe[pentominoe.length-1-i][j] = temp;
+        }
+      }
+    }
+    else{
+      for(int i=0; i<pentominoe.length/2; i++){
+        for(int j=0;j<pentominoe[0].length;j++){
+          char temp = pentominoe[i][j];
+          pentominoe[i][j] = pentominoe[pentominoe.length-1-i][j];
+          pentominoe[pentominoe.length-1-i][j] = temp;
+        }
+      }
+    }
   }
 }
