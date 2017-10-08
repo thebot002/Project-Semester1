152d47954a01183e0e49b7c2372d495769177159
thebot002 <31898693+thebot002@users.noreply.github.com>
2017-10-08 16:55:00 +0200

    Begin Pentominoe definition

--- Pentominoe.java
+++ Pentominoe.java
@@ -1,12 +1,16 @@
 public class Pentominoe {
 
-  private int[][] pentominoe = new int[5][5];
+  private int[][] pentominoe;
 
   public Pentominoe(){
 
   }
-  public Pentominoe(char[] listOfForms, int[] sizeOBoard){
+  public Pentominoe(String form){
+    createPentominoe(form);
+  }
 
+  public int[][] getPentominoe(){
+    return pentominoe;
   }
 
   public void rotate(int amountRotations){
@@ -17,5 +21,109 @@ public class Pentominoe {
   }
   public int[][] hasFlatSideOn(){
 
+  }
+
+  private static void createPentominoe(String form){
+    switch (form) {
+      case "P":
+      pentominoe = new int[2][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+          if(!(i==1 && j==2)) pentominoe[i][j] = 1;
+          else pentominoe[i][j] = 0;
+        }
+      }
+      break;
+      case "x":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+          if(!((i==0 || i==2)&&(j==0 || j==2))) pentominoe[i][j] = 1;
+          else pentominoe[i][j] = 0;
+        }
+      }
+      break;
+      case "f":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+          if(!((i==0) && (j==0 || j==2)) || !()) pentominoe[i][j] = 1;
+          else pentominoe[i][j] = 0;
+        }
+      }
+      break;
+      case "v":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+      case "w":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+      case "y":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+      case "i":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+      case "t":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+      case "z":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+      case "u":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+      case "n":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+      case "l":
+      pentominoe = new int[3][3];
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+
+        }
+      }
+      break;
+    }
   }
 }
