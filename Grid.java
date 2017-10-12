127c78c8ace16a4048cb1f841ab4cbaf03dfd9fc
thebot002 <31898693+thebot002@users.noreply.github.com>
2017-10-12 10:11:15 +0200

    grid object (addition of isFull() & isPossible()) + improvement of add & remove pentominoe methods

--- Grid.java
+++ Grid.java
@@ -38,6 +38,37 @@ public class Grid{
     position[1] = -1;
     return position;
   }
+  public boolean isFull(){
+    boolean check = true;
+    int i = 0;
+    int j = 0;
+    while(i<height){
+      while(j<width){
+        if(grid[i][j] == '-') return false;
+        i++;
+        j++;
+      }
+    }
+    return check;
+  }
+  public boolean isPossible(Pentominoe p, int[] pos){
+    boolean check = false;
+    int x = pos[1]; //j
+    int y = pos[0]; //i
+    int k = 0;
+    while(p.getValue(0,k) == '-'){
+      y--;
+      k++;
+    }
+    if(y<0 || width < (y + p.getWidth()) || height < (x + p.getHeight())) return false; //check if shape goes out of bounds
+    for(int i=0; i<p.getHeight();i++){
+      for(int j=0; j<p.getWidth();j++){
+        if(p.getValue(i,j) != '-' && grid[i][j] == '-') check = true;
+        else return false;
+      }
+    }
+    return check;
+  }
   public void print(){
     for(int i=0; i<height; i++){
       for(int j=0; j<width; j++){
@@ -46,22 +77,35 @@ public class Grid{
       System.out.println("");
     }
   }
-  public void addPentominoe(Pentominoe p, int[] position){
+  public void addPentominoe(Pentominoe p, int[] pos){
+    int x = pos[1]; //j
+    int y = pos[0]; //i
+    int k = 0;
+    while(p.getValue(0,k) == '-'){
+      y--;
+      k++;
+    }
     for(int i=0; i<p.getHeight(); i++){
       for(int j=0; j<p.getWidth(); j++){
-        grid[position[0]+i][position[1]+j] = p.getValue(i,j);
+        if(p.getValue(i,j) != '-') grid[y+i][x+j] = p.getValue(i,j);
       }
     }
   }
   public void removePentominoe(Pentominoe p){
     char x = '-';
-    int posX; //horizontal position to look at;
+    int posX = 0;
     do{
       x = p.getValue(0,posX);
+      posX++;
     }while(x == '-');
+    int blockCounter = 0;
     for(int i=0; i<p.getHeight(); i++){
       for(int j=0; j<p.getWidth(); j++){
-        if(grid[i][j] == x) grid[i][j] = '-';
+        if(grid[i][j] == x){
+          grid[i][j] = '-';
+          blockCounter++;
+        }
+        if(blockCounter == 5) return;
       }
     }
   }
